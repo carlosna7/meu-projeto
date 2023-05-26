a1d8d93241850861b45824e59ba3654fb74d7775
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-05-26 17:11:43 -0300

    Primero dia Att

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './components/HelloWorld';
+import SayMyName from './components/SayMyName';
+import Pessoa from './components/Pessoa';
 
 function App() {
+
+    const nome = "Eduardo"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <SayMyName nome="Carlos" />
+      <SayMyName nome={nome} />
+      <Pessoa 
+        nome="Jorje" 
+        idade="31"
+        profissao="dev"
+        foto="https://via.placeholder.com/150"
+      />
+      
     </div>
   );
 }
---
+++ src/components/SayMyName.js
@@ -0,0 +1,13 @@
+function SayMyName(props) {
+
+    return (
+
+        <div>
+            <p>Olá, {props.nome}! Tudo bem?</p>
+        </div>
+
+    );
+
+};
+
+export default SayMyName;
