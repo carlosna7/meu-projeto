2c9a382717975c75a8d98ecb42ad1b4d16140da9
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-06-14 17:02:09 -0300

    att

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
-import Condicional from './components/Condicional';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import Empresa from "./pages/Empresa";
+import Contato from "./pages/Contato";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>Testando Eventos</h1>
-      <Condicional />
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/empresa" element={<Empresa />} />
+        <Route path="/contato" element={<Contato />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
 export default App;
+
