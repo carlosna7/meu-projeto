4f5902bf4b9bc0d8c7eba2b1eafc0d55a6e7f637
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-06-15 09:43:27 -0300

    att

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,7 +1,23 @@
+import {FaYoutube, FaGoogle, FaFrog} from "react-icons/fa"
+import styles from "./Footer.module.css"
+
 function Footer() {
 
     return (
-        <footer>Rodapé</footer>
+        <footer>
+            <ul className={styles.socialList}>
+                <li>
+                    <FaYoutube />
+                </li>
+                <li>
+                    <FaGoogle />
+                </li>
+                <li>
+                    <FaFrog />
+                </li>
+            </ul>
+            <p>Nosso rodapé</p>
+        </footer>
     )
 
 }
